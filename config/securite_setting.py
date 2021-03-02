ecb650e782fb229d65b9d22bc535de3a9f3a1ceb
jieliuGG <810030709@qq.com>
2021-03-02 23:27:58 +0800

    security_setting.py

---
+++ config/securite_setting.py
@@ -0,0 +1,14 @@
+# -- encoding: utf-8 --
+# @time:    2020/12/14 23:13
+# @Author: jsonLiu
+# @Email: 810030709@qq.com
+# @file: securite_setting.py
+class SecurityCofig:
+    mobile_phone = 18593298080
+    pwd = '12345678'
+
+    admin_mobile_phone = 18593298081
+    admin_pwd = '12345678'
+
+
+s_config = SecurityCofig()
